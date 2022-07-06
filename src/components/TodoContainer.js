ea31be52d9a4bb10491b955b5cdfda798a53edce
waqas ahmad <waqaskanju@gmail.com>
2022-07-05 23:32:10 -0700

    add react life style methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,23 +5,7 @@ import InputTodo from "./InputTodo"
 
 class TodoContainer extends React.Component{
     state = {
-        todos: [
-          {
-            id: 1,
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: 2,
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: 3,
-            title: "Deploy to live server",
-            completed: true
-          }
-        ]
+        todos: [],
        };
        handleChange = id => {
         this.setState(prevState => ({
@@ -64,6 +48,26 @@ class TodoContainer extends React.Component{
             return todo
           }),
         })
+      }
+    /*   componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+          .then(response => response.json())
+          .then(data => this.setState({ todos: data }));
+      } */
+      componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+          const temp = JSON.stringify(this.state.todos)
+          localStorage.setItem("todos", temp)
+        }
+      }
+      componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+          this.setState({
+            todos: loadedTodos
+          })
+        }
       }
     render() {
         return (
